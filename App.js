948f9b4e9219e24230f81f1b77eb696fa9802220
HyoShin <sinto825@naver.com>
2023-05-30 19:56:51 +0900

    사용자 위치 정보에 따른 날씨정보 제공 코드 추가

--- App.js
+++ App.js
@@ -1,33 +1,73 @@
-//위치정보 서비스
+//사용자가 위치하는 좌표정보를 저장하여 그에 따른 날씨정보를 API_KEY를 통해 호출.
+//현재 날씨정보를 제공
+
 import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location';
 
 export default function App() {
   const [location, setLocation] = useState(null);
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();  //위치 엑세스 권한을 요청하기 위한 비동기 함수
-      if (status !== 'granted') {                        //위치 엑세스 권한이 허용되지 않을 시 콘솔창에 출력
-        console.log('위치 권한이 허용되지 않았습니다.');   
-        return;
-      }
+  const [weather, setWeather] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-      let location = await Location.getCurrentPositionAsync({});  //현재 위치정보를 가져오는 비동기 함수
-      setLocation(location);   //location에 위치정보 저장
-    })();
+  useEffect(() => {
+    getLocationAsync();
   }, []);
 
-  //위치권한 허용 시 위도, 경도 출력
-  return (
-    <View>
-      {location ? (
-        <Text>
-          Latitude: {location.coords.latitude}, Longitude: {location.coords.longitude}
-        </Text>
-      ) : (
-        <Text>위치정보를 가져오는 중...</Text>
-      )}
-    </View>
-  );
+  const getLocationAsync = async () => {
+    // 위치 권한 요청
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('위치 권한이 거부되었습니다.');
+      return;
+    }
+
+    // 위치 정보 가져오기
+    const { coords } = await Location.getCurrentPositionAsync();
+    setLocation(coords);
+
+    // 날씨 정보 가져오기
+    fetchWeather(coords.latitude, coords.longitude);
+  };
+
+  const fetchWeather = async (latitude, longitude) => {
+    const API_KEY = '937d5f5ac02447214fd5363f3aee246c';
+    const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+
+    try {
+      const response = await fetch(weatherAPI);
+      const data = await response.json();
+      setWeather(data.weather[0]);
+    } catch (error) {
+      console.error('날씨 정보를 가져오는 중 오류가 발생했습니다.', error);
+    }
+  };
+
+  if (errorMsg) {
+    return (
+      <View style={styles.container}>
+        <Text>{errorMsg}</Text>
+      </View>
+    );
+  } else if (weather) {
+    return (
+      <View style={styles.container}>
+        <Text>현재 날씨: {weather.description}</Text>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>날씨 정보를 가져오는 중...</Text>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
