ea8c2ced87d004f5d577b1e0e1679fb1f862447f
HyoShin <sinto825@naver.com>
2023-06-01 19:28:09 +0900

    로컬맵 구현

--- App.js
+++ App.js
@@ -1,73 +1,17 @@
-//사용자가 위치하는 좌표정보를 저장하여 그에 따른 날씨정보를 API_KEY를 통해 호출.
-//현재 날씨정보를 제공
-
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import * as Location from 'expo-location';
+import MapScreen from './MapScreen';
 
 export default function App() {
-  const [location, setLocation] = useState(null);
-  const [weather, setWeather] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-
-  useEffect(() => {
-    getLocationAsync();
-  }, []);
-
-  const getLocationAsync = async () => {
-    // 위치 권한 요청
-    const { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      setErrorMsg('위치 권한이 거부되었습니다.');
-      return;
-    }
-
-    // 위치 정보 가져오기
-    const { coords } = await Location.getCurrentPositionAsync();
-    setLocation(coords);
-
-    // 날씨 정보 가져오기
-    fetchWeather(coords.latitude, coords.longitude);
-  };
-
-  const fetchWeather = async (latitude, longitude) => {
-    const API_KEY = '937d5f5ac02447214fd5363f3aee246c';
-    const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
-
-    try {
-      const response = await fetch(weatherAPI);
-      const data = await response.json();
-      setWeather(data.weather[0]);
-    } catch (error) {
-      console.error('날씨 정보를 가져오는 중 오류가 발생했습니다.', error);
-    }
-  };
-
-  if (errorMsg) {
-    return (
-      <View style={styles.container}>
-        <Text>{errorMsg}</Text>
-      </View>
-    );
-  } else if (weather) {
-    return (
-      <View style={styles.container}>
-        <Text>현재 날씨: {weather.description}</Text>
-      </View>
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        <Text>날씨 정보를 가져오는 중...</Text>
-      </View>
-    );
-  }
+  return (
+    <View style={styles.container}>
+      <MapScreen />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 });
---
+++ MapScreen.js
@@ -0,0 +1,78 @@
+//npm install axios
+//expo install expo-location 패키지 설치
+//로컬맵 구현 코드
+//사용자의 위치정보를 기반으로 로컬맵 상에 마커 표시.
+//마커에는 그 장소의 기온과 날씨 표시
+
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+import axios from 'axios';
+
+const MapScreen = () => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied'); //위치권한 허용 안할시 발생
+        return;
+      }
+
+      let { coords } = await Location.getCurrentPositionAsync({});
+      setLocation(coords);
+
+      try {
+        const response = await axios.get(
+          `http://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=937d5f5ac02447214fd5363f3aee246c&units=metric`
+        );
+
+        if (response.status === 200) {
+          setWeather(response.data);
+        } else {
+          setErrorMsg('Failed to fetch weather data');
+        }
+      } catch (error) {
+        console.log(error);
+        setErrorMsg('Failed to fetch weather data');
+      }
+    })();
+  }, []);
+
+  const initialRegion = {
+    latitude: location ? location.latitude : 37.566, // 초기 지도 중심 위치의 위도 (대한민국 서울)
+    longitude: location ? location.longitude : 126.978, // 초기 지도 중심 위치의 경도 (대한민국 서울)
+    latitudeDelta: 0.0922, // 지도 영역의 세로 길이
+    longitudeDelta: 0.0421, // 지도 영역의 가로 길이
+  };
+
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} initialRegion={initialRegion}>
+        {location && (
+          <Marker
+            coordinate={{ latitude: location.latitude, longitude: location.longitude }}
+            title="현재 위치"
+            description={weather ? `${weather.main.temp}°C, ${weather.weather[0].description}` : '날씨 정보를 불러오는 중...'}
+          />
+        )}
+        {/* 추가적인 마커들을 원하는 만큼 추가할 수 있습니다 */}
+      </MapView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
+
+export default MapScreen;
