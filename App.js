9975def8e9d98d2dc0566786fe711b8f0027bb5b
apiod <trtxcrop4893@naver.com>
2023-06-04 17:12:00 +0900

    DatePickerExample

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapScreen from './MapScreen';
+import DatePicker from './DatePicker' ;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MapScreen />
+      
+      <DatePicker />
     </View>
   );
 }
