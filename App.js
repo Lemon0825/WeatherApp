9a7e7f914853791db934e39b5bdd5e6d02b533f6
HyoShin <sinto825@naver.com>
2023-06-08 16:49:11 +0900

    날씨정보 표시 코드 수정(axios 패키지 설치)

--- App.js
+++ App.js
@@ -1,69 +1,61 @@
-//사용자가 위치하는 좌표정보를 저장하여 그에 따른 날씨정보를 API_KEY를 통해 호출.
-//현재 날씨정보를 제공
+//npm install axios 패키지 설치
 
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location';
-import DatePicker from "./DatePicker"
+import axios from 'axios';
 
 export default function App() {
   const [location, setLocation] = useState(null);
   const [weather, setWeather] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
-    getLocationAsync();
+    fetchLocation();
   }, []);
 
-  const getLocationAsync = async () => {
-    // 위치 권한 요청
-    const { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      setErrorMsg('위치 권한이 거부되었습니다.');
-      return;
+  const fetchLocation = async () => {
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.error('위치권한이 거부되었습니다.');
+        return;
+      }
+
+      const { coords } = await Location.getCurrentPositionAsync();
+      const { latitude, longitude } = coords;
+
+      setLocation({ latitude, longitude });
+      fetchWeather(latitude, longitude);
+    } catch (error) {
+      console.error('Error fetching location', error);
     }
-
-    // 위치 정보 가져오기
-    const { coords } = await Location.getCurrentPositionAsync();
-    setLocation(coords);
-
-    // 날씨 정보 가져오기
-    fetchWeather(coords.latitude, coords.longitude);
   };
 
   const fetchWeather = async (latitude, longitude) => {
-    const API_KEY = '937d5f5ac02447214fd5363f3aee246c';
-    const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
-
     try {
-      const response = await fetch(weatherAPI);
-      const data = await response.json();
-      setWeather(data.weather[0]);
+      const response = await axios.get(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=937d5f5ac02447214fd5363f3aee246c&units=metric&lang=kr`
+      );
+
+      setWeather(response.data);
     } catch (error) {
       console.error('날씨 정보를 가져오는 중 오류가 발생했습니다.', error);
     }
   };
 
-  if (errorMsg) {
-    return (
-      <View style={styles.container}>
-        <Text>{errorMsg}</Text>
-      </View>
-    );
-  } else if (weather) {
-    return (
-      <View style={styles.container}>
-        <Text>현재 날씨: {weather.description}</Text>
-        <DatePicker/>
-      </View>
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        <Text>날씨 정보를 가져오는 중...</Text>
-      </View>
-    );
-  }
+  return (
+    <View style={styles.container}>
+      {weather ? (
+        <View>
+          <Text>City: {weather.name}</Text>
+          <Text>Temperature: {weather.main.temp}°C</Text>
+          <Text>Description: {weather.weather[0].description}</Text>
+        </View>
+      ) : (
+        <Text>날씨정보를 가져오는 중...</Text>
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
