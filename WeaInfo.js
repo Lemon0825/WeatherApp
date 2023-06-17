08402f9625e0834a3e1c49cf412b9ecfdd54c381
HyoShin <sinto825@naver.com>
2023-06-18 02:41:53 +0900

    추가날씨 이미지, 스크롤바 추가

--- WeaInfo.js
+++ WeaInfo.js
@@ -5,12 +5,12 @@
 //날씨 이미지 추가
 
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
 import * as Location from 'expo-location';
 import axios from 'axios';
 import moment from 'moment';
 
-const WeaInfo = () => {
+const WeatherInfo = () => {
   const [location, setLocation] = useState(null);
   const [weather, setWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
@@ -45,7 +45,7 @@ const WeaInfo = () => {
           `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=937d5f5ac02447214fd5363f3aee246c&units=metric`
         ),
         axios.get(
-          `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=937d5f5ac02447214fd5363f3aee246c&units=metric&lang=kr`
+          `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=937d5f5ac02447214fd5363f3aee246c&units=metric`
         ),
         axios.get(
           `https://api.openweathermap.org/data/2.5/air_pollution?lat=${latitude}&lon=${longitude}&appid=937d5f5ac02447214fd5363f3aee246c`
@@ -60,7 +60,16 @@ const WeaInfo = () => {
     }
   };
 
-  ////날씨 이미지
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.infoContainer}>
+        <Text style={styles.infoKey}>{item.key}</Text>
+        <Text style={styles.infoValue}>{item.value}</Text>
+      </View>
+    );
+  };
+  
+  ////현재 날씨
   const weatherImages = {
     'clear sky': require('./assets/clear.png'),
 
@@ -90,6 +99,73 @@ const WeaInfo = () => {
     'smoke': require('./assets/mist.png'),
   };
 
+  const renderForecastItem = ({ item }) => {
+    let weatherIcon;
+
+    switch (item.weatherDescription.toLowerCase()) {
+      case 'clear sky':
+        weatherIcon = require('./assets/clear.png');
+        break;
+      case 'light rain':
+      case 'moderate rain':
+      case 'heavy rain':
+      case 'drizzle':
+      case 'light drizzle':
+      case 'moderate drizzle':
+        weatherIcon = require('./assets/rain.png');
+        break;
+      case 'thunderstorm with light rain':
+      case 'thunderstorm with rain':
+      case 'thunderstorm with heavy rain':
+        weatherIcon = require('./assets/thunder.png');
+        break;
+      case 'few clouds':
+      case 'scattered clouds':
+      case 'broken clouds':
+      case 'overcast clouds':
+        weatherIcon = require('./assets/sun_cloud.png');
+        break;
+      case 'light snow':
+      case 'moderate snow':
+      case 'heavy snow':
+        weatherIcon = require('./assets/snow.png');
+        break;
+      case 'mist':
+      case 'fog':
+      case 'haze':
+      case 'smoke':
+        weatherIcon = require('./assets/mist.png');
+        break;
+    }
+
+    return (
+      <View style={styles.forecastItem}>
+        <Text style={styles.forecastDate}>{moment(item.date).format('MM월 DD일')}</Text>
+        <Image source={weatherIcon} style={styles.weatherIcon} />
+        <Text>최고 기온: {item.maxTemp}°C 최저 기온: {item.minTemp}°C</Text>             
+      </View>
+    );
+  };
+
+  const renderWeatherInfo = () => {
+    const data = [
+      { key: '위치', value: weather.name },
+      { key: '현재 온도', value: `${weather.main.temp}°C` },
+      { key: '현재 날씨', value: weather.weather[0].description },
+      { key: '습도', value: `${weather.main.humidity}%` },
+      { key: '풍속', value: `${weather.wind.speed} m/s` },
+      { key: '미세먼지', value: `${pollution.list[0].components.pm2_5} μg/m³` }
+    ];
+
+    return (
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    );
+  };
+
   const renderForecast = () => {
     if (!forecast) {
       return null;
@@ -101,53 +177,97 @@ const WeaInfo = () => {
       .filter((date, index, self) => date !== currentDate && self.indexOf(date) === index)
       .slice(0, 5);
 
-    return forecastDates.map((date) => {
+    const forecastData = forecastDates.map((date) => {
       const forecastItems = forecast.list.filter((item) => moment(item.dt_txt).format('YYYY-MM-DD') === date);
 
-      // 최대 기온과 최소 기온
       const maxTemp = Math.max(...forecastItems.map((item) => item.main.temp_max));
       const minTemp = Math.min(...forecastItems.map((item) => item.main.temp_min));
+      const representativeWeather = forecastItems[0].weather[0].description;
 
-      // 대표 날씨 정보
-      const representativeWeather = forecastItems[0];
-
-      return (
-        <View key={date}>
-          <Text>{moment(date).format('MM월 DD일')}  {representativeWeather.weather[0].description}   최고 기온: {maxTemp}°C, 최저 기온: {minTemp}°C</Text>
-          <Text></Text>
-        </View>
-      );
+      return {
+        date,
+        maxTemp,
+        minTemp,
+        weatherDescription: representativeWeather
+      };
     });
+
+    return (
+      <FlatList
+        data={forecastData}
+        renderItem={renderForecastItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    );
   };
 
   return (
     <View style={styles.container}>
       {weather && pollution ? (
-        <View>
-          <Image source={weatherImages[weather.weather[0].description]} style={{ width: 100, height: 100 }}/>
-          <Text>위치: {weather.name}</Text>
-          <Text>현재 온도: {weather.main.temp}°C</Text>
-          <Text>습도: {weather.main.humidity}%</Text>
-          <Text>풍속: {weather.wind.speed} m/s</Text>
-          <Text>미세먼지: {pollution.list[0].components.pm2_5} μg/m³</Text>
-
+        <View style={styles.weatherContainer}>
+          <Text style={styles.title}>현재 날씨 정보</Text>
+          <Image source={weatherImages[weather.weather[0].description]} style={{ width: 150, height: 150 }}/>
+          {renderWeatherInfo()}
           <Text></Text>
-          <Text>추가 날씨 예보</Text>
+          <Text></Text>
+        
+          <Text style={styles.title}>추가 날씨 예보</Text>
           {renderForecast()}
         </View>
       ) : (
-        <Text>날씨 정보를 가져오는 중...</Text>
+        <Text style={styles.loadingText}>날씨 정보를 가져오는 중...</Text>
       )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#ffffff', // Change this color to set the background color
     alignItems: 'center',
+    justifyContent: 'center',
+  },
+  weatherContainer: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  loadingText: {
+    fontSize: 16,
+    fontStyle: 'italic',
+  },
+  infoContainer: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  infoKey: {
+    flex: 1,
+    fontWeight: 'bold',
+    marginRight: 10,
+  },
+  infoValue: {
+    flex: 1,
+  },
+  forecastItem: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+    marginRight: 20,
+  },
+  forecastDate: {
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  weatherIcon: {
+    width: 50,
+    height: 50,
+    marginBottom: 5,
   },
 });
 
-export default WeaInfo;
+export default WeatherInfo;
+
